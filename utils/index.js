be229837c94b005c84967ea5c4f2d01759795d01
PRIYANKA S <109449825+PriyankagithubS@users.noreply.github.com>
2024-08-21 12:47:56 +0530

    Update index.js

--- utils/index.js
+++ utils/index.js
@@ -5,9 +5,10 @@ export const createJWT = (res, userId) => {
     });
 
     
-  res.cookie("token", token, {
-    httpOnly: true,
-    secure: process.env.NODE_ENV !== "development", // true in production
-    sameSite: "None", // For cross-origin requests
-    maxAge: 1 * 24 * 60 * 60 * 1000, // 1 day
-});
+    res.cookie("token", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV !== "development",
+        sameSite: "strict", 
+        maxAge: 1 * 24 * 60 * 60 * 1000, //1 day
+    });
+};
